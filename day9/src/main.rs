9fdcb5cf1e3c5db1ec468f8b0f8c2021ffb94cb0
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-10 04:26:26 -0500

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,8 +1,50 @@
 use sscanf::sscanf;
 use std::collections::HashSet;
 
+fn tail_positions(head_positions: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut tail = (0, 0);
+    let mut tail_positions = Vec::new();
+
+    for head in head_positions {
+        let horizontal_displacement = head.0 - tail.0;
+        let vertical_displacement = head.1 - tail.1;
+
+        tail = match (horizontal_displacement, vertical_displacement, tail) {
+            ( 2,  1, (x, y)) => (x + 1, y + 1),
+            ( 2,  2, (x, y)) => (x + 1, y + 1),
+            ( 1,  2, (x, y)) => (x + 1, y + 1),
+
+            ( 0,  2, (x, y)) => (x    , y + 1),
+
+            (-1,  2, (x, y)) => (x - 1, y + 1),
+            (-2,  2, (x, y)) => (x - 1, y + 1),
+            (-2,  1, (x, y)) => (x - 1, y + 1),
+
+            (-2,  0, (x, y)) => (x - 1, y    ),
+
+            (-2, -1, (x, y)) => (x - 1, y - 1),
+            (-2, -2, (x, y)) => (x - 1, y - 1),
+            (-1, -2, (x, y)) => (x - 1, y - 1),
+
+            ( 0, -2, (x, y)) => (x    , y - 1),
+
+            ( 1, -2, (x, y)) => (x + 1, y - 1),
+            ( 2, -2, (x, y)) => (x + 1, y - 1),
+            ( 2, -1, (x, y)) => (x + 1, y - 1),
+
+            ( 2,  0, (x, y)) => (x + 1, y    ),
+            
+
+            (_, _, (x, y)) => (x, y),
+        };
+
+        tail_positions.push(tail);
+    }
+
+    tail_positions
+}
+
 fn main() {
-    let input = std::fs::read_to_string("./example.txt").expect("should read file");
     let input = std::fs::read_to_string("./input.txt").expect("should read file");
 
     let motions = input
@@ -12,9 +54,8 @@ fn main() {
         // .map(|r| {println!("{:?}", r); r})
         .map(|r| r.unwrap());
 
-    
     let mut head = (0, 0);
-    let mut head_positions =Vec::new();
+    let mut head_positions = Vec::new();
     for (direction, steps) in motions {
         for _ in 0..steps {
             head = match (head, direction) {
@@ -28,42 +69,31 @@ fn main() {
         }
     }
 
-    let mut tail_spots = HashSet::<(i32, i32)>::new();
-    let mut tail = (0, 0);
+    // Part 1
+    let unique_tail_positions: HashSet<(i32, i32)> =
+        tail_positions(head_positions.clone()).into_iter().collect();
+    println!("part 1: {:?}", unique_tail_positions.len());
 
-    for head in head_positions {
-
-        let horizontal_displacement = head.0 - tail.0;
-        let vertical_displacement = head.1 - tail.1;
-
-        tail = match(horizontal_displacement, vertical_displacement, tail) {
-            (2, 2, (x, y)) => (x + 1, y + 1),
-            (1, 2,(x, y)) => (x + 1, y + 1),
-            (0, 2, (x, y)) => (x, y + 1),
-            (-1, 2, (x, y)) => (x - 1, y +1),
-            (-2, 2, (x, y)) => (x - 1, y + 1),
-            
-            (-2, 1, (x, y)) => (x - 1, y + 1),
-            (-2, 0, (x, y)) => (x - 1, y),
-            (-2, -1, (x, y)) => (x - 1, y - 1),
-
-            (-2, -2, (x, y)) => (x - 1, y - 1),
-            (-1, -2, (x, y)) => (x - 1, y - 1),
-            (0, -2, (x, y)) => (x, y - 1),
-            (1, -2, (x, y)) => (x + 1, y - 1),
-            (2, -2, (x, y)) => (x + 1, y - 1),
-            
-            (2, -1, (x, y)) => (x + 1, y - 1),
-            (2, 0, (x, y)) => (x + 1, y),
-            (2, 1, (x, y)) => (x + 1, y + 1),
-
-            (_, _, (x, y)) => (x, y),
-        };
-
-        println!("Head spot: {:?}\nTail spot: {:?}\n", head, tail);
-        
-        tail_spots.insert(tail);
+    // Part 2
+    let mut positions = head_positions;
+    for _ in 1..10 {
+        // println!("after {} generations: {:?}", gen, positions);
+        positions = tail_positions(positions);
     }
 
-    println!("part 1: {:?}", tail_spots.len());
+    let unique_tail_positions: HashSet<(i32, i32)> =positions.into_iter().collect();
+    println!("part 2: {:?}", unique_tail_positions.len());
+
+
+    // Debug printing for part 2
+    for i in -13i32..13 {
+        for j in -13i32..13 {
+            if unique_tail_positions.contains(&(-i, j)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }
