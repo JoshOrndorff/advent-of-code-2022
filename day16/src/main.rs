abd1f860a60a8260e2cd7a9e20bacb8303b1a4c8
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-17 02:21:04 -0500

    refactor day 16 and use hashmap for moderate speed improvement

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,4 @@
-// use itertools::Itertools;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeSet, HashMap};
 
 #[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 struct ExplorationState {
@@ -29,7 +28,7 @@ fn main() {
     let input = std::fs::read_to_string("./input.txt").expect("Reads input file");
 
     // We model the cave as a mapping from each node name to its flow rate and its neighbors
-    let mut cave: BTreeMap<String, (u64, Vec<String>)> = BTreeMap::new();
+    let mut cave: HashMap<String, (u64, Vec<String>)> = HashMap::new();
     input.trim().lines().for_each(|l| {
         let equal_index = l.find('=').unwrap();
         let semi_index = l.find(';').unwrap();
@@ -47,9 +46,6 @@ fn main() {
         cave.insert(location, (flow, neighbors));
     });
 
-    // The set of states to explore. mapped to the time remaining when exploring them.
-    // If at some point we find a faster way to get to the same state, we don't explore the slower.
-    let mut to_explore = BTreeMap::<ExplorationState, Vec<Status>>::new();
     let starting_state = ExplorationState {
         location: "AA".into(),
         open_valves: BTreeSet::new(),
@@ -58,9 +54,19 @@ fn main() {
         score: 0,
         time_left: 30,
     };
+    
+    let part_1 = explore_valves(&cave, starting_state, starting_status);
+
+    println!("part 1: {:?}", part_1);
+}
+
+fn explore_valves(cave: &HashMap<String, (u64, Vec<String>)>, starting_state: ExplorationState, starting_status: Status) -> u64 {
+    // The set of states to explore. mapped to the time remaining when exploring them.
+    // If at some point we find a faster way to get to the same state, we don't explore the slower.
+    let mut to_explore = HashMap::<ExplorationState, Vec<Status>>::new();
     to_explore.insert(starting_state, vec![starting_status]);
 
-    let mut explored = BTreeMap::<ExplorationState, Vec<Status>>::new();
+    let mut explored = HashMap::<ExplorationState, Vec<Status>>::new();
 
     while !to_explore.is_empty() {
         
@@ -135,66 +141,14 @@ fn main() {
     }
 
     // Get the highest score state out of the explored set.
-    let mut part_1 = 0;
+    let mut result = 0;
     for (_, statuses) in explored {
         for status in statuses {
-            if status.score > part_1 {
-                part_1 = status.score;
+            if status.score > result {
+                result = status.score;
             }
         }
     }
 
-    // let part_1 = find_best_strategy(&cave, 30, 0, starting_state);
-    // // todo find the explored state with the highest score
-
-    println!("part 1: {:?}", part_1);
+    result
 }
-
-// fn find_best_strategy(
-//     cave: &BTreeMap<String, (u64, Vec<String>)>,
-//     time_left: u64,
-//     score: u64,
-//     current_state: ExplorationState,
-// ) -> u64 {
-//     if time_left == 0 {
-//         return score;
-//     }
-
-//     if time_left > 15 {
-//         println!(
-//             "Exploring location {}. Time left: {}, score: {}",
-//             current_state.location, time_left, score
-//         );
-//     }
-
-//     let (flow_rate, neighbor_locations) = &cave[&current_state.location];
-
-//     // Accumulate the best scores for all of the neighbors
-//     let mut neighbor_scores: Vec<u64> = Vec::new();
-
-//     if current_state.local_valve_closed() && *flow_rate > 0 {
-//         let mut open_valved_neighbor = current_state.clone();
-//         open_valved_neighbor
-//             .open_valves
-//             .insert(current_state.location.clone());
-
-//         let open_valved_neighbor_score = find_best_strategy(
-//             cave,
-//             time_left - 1,
-//             score + flow_rate * time_left,
-//             open_valved_neighbor,
-//         );
-//         neighbor_scores.push(open_valved_neighbor_score);
-//     }
-
-//     for neighbor_location in neighbor_locations {
-//         let mut neighbor = current_state.clone();
-//         neighbor.location = neighbor_location.clone();
-
-//         let neighbor_score = find_best_strategy(cave, time_left - 1, score, neighbor);
-//         neighbor_scores.push(neighbor_score);
-//     }
-
-//     // Return the best score available by all the neighbor states
-//     *neighbor_scores.iter().max().unwrap()
-// }
