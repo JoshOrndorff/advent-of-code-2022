d11819937a271a422d5255a8b8f7a63a8d706822
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-17 02:13:03 -0500

    Solve day 16 part 1
    Still took several seconds in cargo run --release, so I wonder if there is a better way

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,10 +1,8 @@
-use itertools::Itertools;
+// use itertools::Itertools;
 use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 struct ExplorationState {
-    // Put score field first so ord sorts them by score
-    score: u64,
     location: String,
     open_valves: BTreeSet<String>,
 }
@@ -12,6 +10,18 @@ struct ExplorationState {
 impl ExplorationState {
     fn local_valve_closed(&self) -> bool {
         !self.open_valves.contains(&self.location)
+    }
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+struct Status {
+    score: u64,
+    time_left: u64,
+}
+
+impl Status {
+    fn dominates(&self, other: &Self) -> bool {
+        self.score >= other.score && self.time_left >= other.time_left
     }
 }
 
@@ -39,91 +49,100 @@ fn main() {
 
     // The set of states to explore. mapped to the time remaining when exploring them.
     // If at some point we find a faster way to get to the same state, we don't explore the slower.
-    let mut to_explore = BTreeMap::<ExplorationState, u64>::new();
+    let mut to_explore = BTreeMap::<ExplorationState, Vec<Status>>::new();
     let starting_state = ExplorationState {
         location: "AA".into(),
         open_valves: BTreeSet::new(),
-        score: 0,
     };
-    to_explore.insert(starting_state, 30);
+    let starting_status = Status {
+        score: 0,
+        time_left: 30,
+    };
+    to_explore.insert(starting_state, vec![starting_status]);
 
-    let mut explored = BTreeMap::<ExplorationState, u64>::new();
+    let mut explored = BTreeMap::<ExplorationState, Vec<Status>>::new();
 
     while !to_explore.is_empty() {
         
-        // Kinda convoluted way to get ownership of a single item from the map
-        // Get the one with the lowest time, so we never have to re-explore a node
+        // Kinda convoluted way to get ownership of a single kv from the map
         let current_state = to_explore
             .iter()
-            .sorted_by(|(_, v1), (_, v2)| Ord::cmp(&v2, &v1))
             .next()
             .map(|(k, _)| k)
             .expect("to_explore is not empty; we checked in the loop condition")
             .clone();
-        let (current_state, time_left) = to_explore
+        let (current_state, statuses) = to_explore
             .remove_entry(&current_state)
             .expect("Item should exist,we just got the reference from the map itself.");
         let (flow_rate, neighbor_locations) = &cave[&current_state.location];
 
-        // println!("{:?}, time_left {:?}", current_state, time_left);
-        // println!(" Curious state is in to_explore: {:?}", to_explore.contains_key(&dd));
+        for Status{time_left, score} in statuses {
+            // println!("{:?}, time_left {:?}", current_state, time_left);
+            // println!(" Curious state is in to_explore: {:?}", to_explore.contains_key(&dd));
 
-        explored.insert(current_state.clone(), time_left);
+            explored
+                .entry(current_state.clone())
+                .and_modify(|v| v.push(Status{time_left, score}))
+                .or_insert(vec![Status{time_left, score}]);
 
-        if time_left == 0 {
-            continue;
-        }
-
-        let time_left = time_left - 1;
-
-        // Figure out all the neighboring states.
-        let mut neighbors = Vec::new();
-
-        if current_state.local_valve_closed() && *flow_rate > 0 {
-            let mut open_valved_neighbor = current_state.clone();
-            open_valved_neighbor
-                .open_valves
-                .insert(current_state.location.clone());
-            open_valved_neighbor.score = current_state.score + flow_rate * time_left;
-
-            neighbors.push(open_valved_neighbor);
-        }
-
-        for neighbor_location in neighbor_locations {
-            // println!("  neighbor location: {}", neighbor_location);
-            let mut neighbor = current_state.clone();
-            neighbor.location = neighbor_location.clone();
-
-            neighbors.push(neighbor);
-        }
-
-
-
-        // Add the neighbors to the states to explore, unless it is already there or
-        // already explored
-        for neighbor in neighbors {
-            if explored.contains_key(&neighbor) {
+            if time_left == 0 {
                 continue;
             }
 
-            match to_explore.get(&neighbor) {
-                None => {
-                    // println!("  Inserting {:?}", neighbor);
-                    to_explore.insert(neighbor, time_left);
-                    // println!("   Curious state is in to_explore: {:?}", to_explore.contains_key(&dd));
+            let time_left = time_left - 1;
+
+            // Figure out all the neighboring states.
+            let mut neighbors = Vec::new();
+
+            if current_state.local_valve_closed() && *flow_rate > 0 {
+                let mut open_valved_neighbor = current_state.clone();
+                open_valved_neighbor
+                    .open_valves
+                    .insert(current_state.location.clone());
+
+                neighbors.push((open_valved_neighbor, Status { score: score + flow_rate * time_left, time_left}));
+            }
+
+            for neighbor_location in neighbor_locations {
+                // println!("  neighbor location: {}", neighbor_location);
+                let mut neighbor = current_state.clone();
+                neighbor.location = neighbor_location.clone();
+
+                neighbors.push((neighbor, Status{score, time_left}));
+            }
+
+
+
+            // Add the neighbors to the states to explore, unless better ones are already
+            // there or it is already explored
+            for (neighbor, status) in neighbors {
+                if explored.get(&neighbor).unwrap_or(&Vec::new()).iter().any(|exp| exp.dominates(&status)) {
+                    continue;
                 }
-                Some(&previous_time_left) => {
-                    if time_left < previous_time_left {
-                        to_explore.insert(neighbor, time_left);
-                    }
+
+                if to_explore.get(&neighbor).unwrap_or(&Vec::new()).iter().any(|exp| exp.dominates(&status)) {
+                    continue;
                 }
+
+                // None of the explored or to-be-explored entries dominate this one, so we will plan to explore it
+                // Schedule its exploration, and cull the exploration of any statuses it dominates
+                to_explore.entry(neighbor).and_modify(|v| {
+                    v.retain(|s| !status.dominates(s));
+                    v.push(status.clone());
+                }).or_insert(vec![status]);
             }
         }
     }
 
     // Get the highest score state out of the explored set.
-    // TODO Check this. It should be last because it is a sorted map, and score is most important in sorting states
-    let part_1 = explored.keys().rev().next().unwrap().score;
+    let mut part_1 = 0;
+    for (_, statuses) in explored {
+        for status in statuses {
+            if status.score > part_1 {
+                part_1 = status.score;
+            }
+        }
+    }
 
     // let part_1 = find_best_strategy(&cave, 30, 0, starting_state);
     // // todo find the explored state with the highest score
