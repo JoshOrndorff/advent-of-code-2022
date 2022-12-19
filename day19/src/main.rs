a59f997b3bb4c5a05df2d3da29428e41d1b8b530
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-19 10:13:38 -0500

    "solve" day 19 part 2, but this code is slow af and ugly too

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -2,7 +2,7 @@
 // 17472 is too low
 
 use sscanf::sscanf;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 type Quad = (u64, u64, u64, u64);
 
@@ -48,21 +48,23 @@ impl Blueprint {
         println!("Blueprint is {:?}", self);
 
         let starting_state = State {
-            bots: (1, 0, 0, 0),
             stuff: (0, 0, 0, 0),
+            bots: (1, 0, 0, 0),
         };
 
         let mut current_generation = HashSet::from([starting_state]);
         let mut time_left = time;
 
+        let mut lineage = HashMap::new();
+
         while time_left > 0 {
-            println!("Time left: {}", time_left);
             let mut next_generation = HashSet::new();
+            println!("Time left: {}. Generation size: {}", time_left, current_generation.len());
 
             for current_state in &current_generation {
-                // println!("  Current state: {:?}", current_state);
 
                 let children = current_state.get_next_states(self);
+                // print!("  Current state: {:?}", current_state);
                 for child in children {
                     // Check if this one is dominated by any already-scheduled. If so, ditch it, otherwise, cull and add
                     if next_generation.iter().any(|ns: &State| ns.dominates(&child)) {
@@ -70,15 +72,33 @@ impl Blueprint {
                     } else {
                         next_generation.retain(|ns| !child.dominates(ns));
                         next_generation.insert(child);
+                        lineage.insert(child, current_state.clone());
                     }
                 }
+                // println!(" Next generation: {:?}", next_generation.len());
             }
 
             current_generation = next_generation;
             time_left -= 1;
         }
 
+
         current_generation.into_iter().fold(0, |acc, s| if s.stuff.3 > acc { s.stuff.3 }else { acc })
+        // let mut most_geodes = 0;
+        // let mut noted_state = State{ bots: (0,0,0,0), stuff: (0,0,0,0)};
+        // for s in current_generation {
+        //     if s.stuff.3 > most_geodes {
+        //         most_geodes = s.stuff.3;
+        //         noted_state = s;
+        //     }
+        // }
+
+        // while noted_state != starting_state {
+        //     println!("{:?}", noted_state);
+        //     noted_state = lineage[&noted_state];
+        // }
+
+        // most_geodes
     }
 }
 
@@ -98,34 +118,26 @@ impl State {
     fn dominates(&self, other: &Self) -> bool {
 
         // Attempt 2: More geodes is always better.
-        // if self.stuff.3 > other.stuff.3 {
-        //     return true;
-        // } else if other.stuff.3 > self.stuff.3 {
-        //     return false;
-        // }
+        if self.stuff.3 > other.stuff.3 + 2 {
+            return true;
+        }
 
-        // // Having compared geodes, let's now compare geode bots
-        // // Out of curiosity, I tried commenting this out, but it made no difference
-        // //I also tried putting this comparison ahead of the other one, but, again, no effect
-        // if self.bots.3 > other.bots.3 {
-        //     return true;
-        // } else if other.bots.3 > self.bots.3 {
-        //     return false;
-        // }
+        // Having compared geodes, let's now compare geode bots
+        // Out of curiosity, I tried commenting this out, but it made no difference
+        //I also tried putting this comparison ahead of the other one, but, again, no effect
+        if self.bots.3 > other.bots.3 + 1 {
+            return true;
+        }
 
         // I had considered doing a similar comparison for the obsidian, but it fails
         // to find the best solutions for my real input.
-        // if self.stuff.2 > other.stuff.2 {
+        // if self.stuff.2 > other.stuff.2 + 1 {
         //     return true;
-        // } else if other.stuff.2 > self.stuff.2 {
-        //     return false;
         // }
 
-        // if self.bots.2 > other.bots.2 {
-        //     return true;
-        // } else if other.bots.2 > self.bots.2 {
-        //     return false;
-        // }
+        if self.bots.2 > other.bots.2 + 3 {
+            return true;
+        }
 
         // This was my simple first attempt. Let's retain it as the ultimate fallback
         quad_covers(self.stuff, other.stuff) && quad_covers(self.bots, other.bots)
@@ -171,7 +183,7 @@ impl State {
         // See if we can build another geode bot
         if quad_covers(self.stuff, blueprint.geode_bot_cost) {
             let geode_child = State {
-              bots: add_quad(template_child.bots, (1, 0, 0, 1)),
+              bots: add_quad(template_child.bots, (0, 0, 0, 1)),
               stuff: sub_quad(template_child.stuff, blueprint.geode_bot_cost),
             };
             children.push(geode_child);
