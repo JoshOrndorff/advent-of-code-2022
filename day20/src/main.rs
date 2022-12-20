df7f79e64bf4ca57e72b3d2a6038b3a25e157d66
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-20 01:31:24 -0500

    Solve day 20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,16 +1,48 @@
-// use itertools::Itertools;
-// use sscanf::sscanf;
-// use std::collections::{HashSet, HashMap, VecDeque};
+const KEY: i64 = 811589153;
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt").expect("read input file");
 
     // The data is stored as a vec of pairs. The pair is the original position then the value
-    let mut data: Vec<(usize, i64)> = input
+    let raw_data: Vec<(usize, i64)> = input
         .lines()
         .map(|l| l.parse().unwrap())
-        .enumerate() /*.map(|(n, v)| (n as i64, v))*/
+        .enumerate()
         .collect();
+
+    // Part 1
+    let mut data = raw_data.clone();
+    mix_one_round(&mut data);
+    let part_1 = calculate_coordinates(&data);
+    println!("Part 1 {:?}", part_1);
+
+    // Part 2
+    let mut data: Vec<(usize, i64)> = raw_data.iter().map(|(n, v)| (*n, v * KEY)).collect();
+    for _i in 0..10 {
+        mix_one_round(&mut data);
+        // println!("Data after round {}: \n{:?}\n", _i, data.iter().map(|(_,v)| v).collect::<Vec<_>>());
+    }
+    let part_2 = calculate_coordinates(&data);
+    println!("Part 2 {:?}", part_2);
+}
+
+fn calculate_coordinates(data: &Vec<(usize, i64)>) -> i64 {
+    let length = data.iter().count();
+
+    let mut zero_index = 0;
+    for search_loc in 0..length {
+        if data[search_loc].1 == 0 {
+            zero_index = search_loc;
+            break;
+        }
+    }
+
+    data[(zero_index + 1000) % length].1
+        + data[(zero_index + 2000) % length].1
+        + data[(zero_index + 3000) % length].1
+}
+
+fn mix_one_round(data: &mut Vec<(usize, i64)>) {
     let length = data.iter().count();
 
     for i in 0..length {
@@ -24,7 +56,7 @@ fn main() {
             }
         }
 
-        let ending_position = ((starting_position as i64 + 2 * (length as i64 - 1) + value)
+        let ending_position = ((starting_position as i64 + 10 * KEY * (length as i64 - 1) + value)
             % (length as i64 - 1)) as usize;
         let in_transit = data.remove(starting_position);
 
@@ -34,21 +66,5 @@ fn main() {
         } else {
             data.insert(ending_position, in_transit);
         }
-
-        // println!("Data after round {}: {:?}", i, data.iter().map(|(_,v)| v).collect::<Vec<_>>());
     }
-
-    let mut zero_index = 0;
-    for search_loc in 0..length {
-        if data[search_loc].1 == 0 {
-            zero_index = search_loc;
-            break;
-        }
-    }
-
-    let part_1 = data[(zero_index + 1000) % length].1
-        + data[(zero_index + 2000) % length].1
-        + data[(zero_index + 3000) % length].1;
-
-    println!("Part 1 {:?}", part_1);
 }
