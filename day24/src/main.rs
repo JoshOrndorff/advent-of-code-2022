4f29dd9b310d2dc6a609777e8014c548120e636f
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-24 03:16:04 -0500

    Solve day 24 part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, VecDeque, HashSet};
+use std::collections::{BTreeMap, HashSet, VecDeque};
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt").expect("read input file");
@@ -37,20 +37,42 @@ fn main() {
         })
     });
 
-    let destination = (end_x, height - 1);
+    let entrance = (start_x, 0usize);
+    let exit = (end_x, height - 1);
 
+    let (first_pass_steps, blizzards) = traverse_valley(entrance, exit, width, height, blizzards);
+    let (second_pass_steps, blizzards) = traverse_valley(exit, entrance, width, height, blizzards);
+    let (third_pass_steps, _) = traverse_valley(entrance, exit, width, height, blizzards);
+
+    // CAUTION! There is an off-by-one issue between my results and the AOC solutions in subsequent crossings. I'm not sure why.
+    println!("Traversed the valley in {first_pass_steps}");
+    println!("Returned in {}", second_pass_steps + 1);
+    println!("Traversed again in {}", third_pass_steps + 1);
+
+    println!(
+        "\nTotal journey distance: {}",
+        first_pass_steps + second_pass_steps + third_pass_steps + 2
+    );
+}
+
+fn traverse_valley(
+    start: (usize, usize),
+    destination: (usize, usize),
+    width: usize,
+    height: usize,
+    mut blizzards: BTreeMap<(usize, usize), Vec<char>>,
+) -> (usize, BTreeMap<(usize, usize), Vec<char>>) {
     // We track our x, y coordinates through the valley on a step-by-step basis
     // Each "generation" represents one step in our path.
-    let mut current_generation = VecDeque::from([(start_x, 0usize)]);
+    let mut current_generation = VecDeque::from([start]);
     let mut steps = 0usize;
 
     // Keep track of states we've already seen in case there are ways to wander in circles
-    let mut explored: HashSet<(usize, usize, BTreeMap<(usize, usize), Vec<char>>)> = HashSet::new();
+    let mut explored: HashSet<(usize, usize, u64)> = HashSet::new();
 
-    'outer: loop {
-
-        println!("\n\nstep {steps}:");
-        println!("generation size: {}", current_generation.len());
+    loop {
+        // println!("\n\nstep {steps}:");
+        // println!("generation size: {}", current_generation.len());
 
         // // For debugging purposes, print the blizzards
         // for y in 0..height {
@@ -69,7 +91,6 @@ fn main() {
         //     println!();
         // }
 
-
         // A new queue for the next generations of places we may trek
         let mut next_generation: VecDeque<(usize, usize)> = VecDeque::new();
 
@@ -78,17 +99,16 @@ fn main() {
             step_blizzards(width, height, &blizzards);
 
         for (x, y) in &current_generation {
-
             if (*x, *y) == destination {
-                println!("reached destination in {steps} steps");
-                break 'outer;
+                return (steps, next_blizzards);
             }
 
             // See if we've explored this state before. If so, bail, if not, note it for future
-            if explored.contains(&(*x, *y, next_blizzards.clone())) {
-                continue
+            let h = hash(&next_blizzards);
+            if explored.contains(&(*x, *y, h)) {
+                continue;
             } else {
-                explored.insert((*x, *y, next_blizzards.clone()));
+                explored.insert((*x, *y, h));
             }
 
             // Check the five possible move options
@@ -104,7 +124,8 @@ fn main() {
                 next_generation.push_back((x - 1, *y))
             }
 
-            if !next_blizzards.contains_key(&(*x, y + 1)) {
+            // We need an extra condition here so we don't step off the bottom when beginning the return journey
+            if *y < height - 1 && !next_blizzards.contains_key(&(*x, y + 1)) {
                 next_generation.push_back((*x, y + 1));
             }
 
@@ -119,7 +140,6 @@ fn main() {
         current_generation = next_generation;
         blizzards = next_blizzards;
     }
-
 }
 
 fn step_blizzards(
@@ -170,11 +190,18 @@ fn step_blizzards(
                 }
                 '#' => {
                     next.insert((*x, *y), vec!['#']);
-                },
+                }
                 bad_direction => panic!("encountered bad direction: {bad_direction}"),
             }
         }
     }
 
     next
+}
+
+use std::hash::{Hash, Hasher};
+fn hash<T: Hash>(t: &T) -> u64 {
+    let mut s = std::collections::hash_map::DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
